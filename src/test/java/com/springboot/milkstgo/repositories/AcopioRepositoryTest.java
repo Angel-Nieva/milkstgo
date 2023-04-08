9902b57d2e22239d1aea829abcb119dfea1446da
Angel <angel.nieva@usach.cl>
2023-04-07 23:30:15 -0400

    test acopioRepository

--- src/test/java/com/springboot/milkstgo/repositories/AcopioRepositoryTest.java
+++ src/test/java/com/springboot/milkstgo/repositories/AcopioRepositoryTest.java
@@ -2,60 +2,119 @@ package com.springboot.milkstgo.repositories;
 
 import com.springboot.milkstgo.entities.AcopioEntity;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
-@DataJpaTest
+@SpringBootTest
 class AcopioRepositoryTest {
 
     @Autowired
-    private AcopioRepository acopioRepository;
+    AcopioRepository acopioRepository;
 
-    private AcopioEntity acopio;
+    private AcopioEntity acopio1;
+    private AcopioEntity acopio2;
 
     @BeforeEach
     void setUp() throws ParseException {
-        AcopioEntity acopio = new AcopioEntity();
+        acopio1 = new AcopioEntity();
+        acopio2 = new AcopioEntity();
         SimpleDateFormat formato_fecha = new SimpleDateFormat("yyyy/MM/dd");
-        acopio.setFecha(formato_fecha.parse("2023/03/17"));
-        acopio.setTurno("M");
-        acopio.setProveedor("13001");
-        acopio.setKls_leche("50");
+        acopio1.setFecha(formato_fecha.parse("2023/03/17"));
+        acopio1.setTurno("M");
+        acopio1.setProveedor("13001");
+        acopio1.setKls_leche("50");
+
+        acopio2.setFecha(formato_fecha.parse("2023/03/17"));
+        acopio2.setTurno("T");
+        acopio2.setProveedor("13001");
+        acopio2.setKls_leche("40");
     }
 
+    @DisplayName("Test para calcular los Kilos de leche de un acopio ")
     @Test
-    void klsLecheByproveedor() {
+    void klsLecheByproveedor(){
         //Given
-        acopioRepository.save(acopio);
+        acopioRepository.save(acopio1);
+        acopioRepository.save(acopio2);
         //When
-        Integer kls_leche = acopioRepository.klsLecheByproveedor(acopio.getProveedor());
-        assertEquals(50, kls_leche);
-        acopioRepository.delete(acopio);
+        Integer kls_leche = acopioRepository.klsLecheByproveedor(acopio1.getProveedor());
+        //Then
+        assertEquals(90, kls_leche);
+        acopioRepository.delete(acopio1);
+        acopioRepository.delete(acopio2);
     }
 
+    @DisplayName("Test para calcular los acopios de un proveedor en la mañana")
     @Test
     void envioProveedorManana() {
+        //Given
+        acopioRepository.save(acopio1);
+        //When
+        Integer enviosManana = acopioRepository.envioProveedorManana(acopio1.getProveedor());
+        //Then
+        assertEquals(1, enviosManana);
+        acopioRepository.delete(acopio1);
     }
-
+    @DisplayName("Test para calcular los acopios de un proveedor en la tarde")
     @Test
     void envioProveedorTarde() {
+        //Given
+        acopioRepository.save(acopio2);
+        //When
+        Integer enviosTarde = acopioRepository.envioProveedorTarde(acopio2.getProveedor());
+        //Then
+        assertEquals(1, enviosTarde);
+        acopioRepository.delete(acopio2);
     }
 
+    @DisplayName("Test para calcular la quincena del acopio")
     @Test
     void quincenaByProveedor() {
+        //Given
+        acopioRepository.save(acopio1);
+        SimpleDateFormat formato_fecha = new SimpleDateFormat("yyyy-MM-dd");
+
+        //When
+        Date enviosTarde = acopioRepository.quincenaByProveedor(acopio1.getProveedor());
+        String a = formato_fecha.format(enviosTarde);
+        //Then
+        assertEquals("2023-03-17", a);
+        acopioRepository.delete(acopio1);
     }
 
+    @DisplayName("Test para calcular los dias en un acopio de un proveedor")
     @Test
     void diasEnvioLeche() {
+        //Given
+        acopioRepository.save(acopio1);
+        acopioRepository.save(acopio2);
+
+        //When
+        int dias = acopioRepository.diasEnvioLeche(acopio1.getProveedor());
+        //Then
+        assertEquals(1, dias);
+        acopioRepository.delete(acopio1);
+        acopioRepository.delete(acopio2);
     }
 
     @Test
     void avgEnvioLeche() {
+        //Given
+        acopioRepository.save(acopio1);
+        acopioRepository.save(acopio2);
+
+        //When
+        int avgLecheDiaria = acopioRepository.avgEnvioLeche(acopio1.getProveedor());
+        //Then
+        assertEquals(90, avgLecheDiaria);
+        acopioRepository.delete(acopio1);
+        acopioRepository.delete(acopio2);
     }
 }
